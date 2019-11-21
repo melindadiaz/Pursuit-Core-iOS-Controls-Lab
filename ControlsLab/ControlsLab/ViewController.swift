cb132d19748ad3524012143cf0cd440299a86ca0
melinda <melindad_82@msn.com>
2019-11-21 13:14:57 -0500

    Started implementing segmented controls

--- ControlsLab/ControlsLab/ViewController.swift
+++ ControlsLab/ControlsLab/ViewController.swift
@@ -2,9 +2,29 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+   lazy var segmentedControl = UISegmentedControl = {
+        let sc = UISegmentedControl(items: ["♠️", "♣️", "♥️", "♦️"])
+        return sc
+        sc.selectedSegmentIndex = 0
+        sc.addTarget(self, action: #selector(handleSegmentedChange), for: ValueChanged)
+    }()
+    
+    @objc fileprivate func handledSegmentChange() {
+        
+    }
+    
+    let club = [String]()
+    let spade = [String]()
+    let heart = [String]()
+    let diamond = [String]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    
     }
+    
+    //let stackView = UIStackView(arrangedSubviews: UISegmentedControl)
+  
 }
 
---
+++ ControlsLab/ControlsLab/segmentControls.swift
@@ -0,0 +1,41 @@
+//
+//  segmentControls.swift
+//  ControlsLab
+//
+//  Created by Melinda Diaz on 11/21/19.
+//  Copyright © 2019 Benjamin Stone. All rights reserved.
+//
+
+import UIKit
+
+//@IBDesignable class segmentControls: UIControl {
+//
+//    private var labels = UIControl()
+//    var items: [String] = ["suit1", "suit2", "suit3", "suit4"] {
+//        didSet {
+//            setupLabels()
+//        }
+//    }
+//    func setupLabels() {
+//        for label in labels {
+//            label.removeFromSuperview()
+//        }
+//        labels.removeAll(keepCapacity = true)
+//
+//        for index in 1...items.count {
+//            let label = UILabel(frame: CGRectZero)
+//        }
+//    }
+//
+//    override func awakeFromNib() {
+//        super.awakeFromNib()
+//        // Initialization code
+//    }
+
+//    override func setSelected(_ selected: Bool, animated: Bool) {
+//        super.setSelected(selected, animated: animated)
+//
+//        // Configure the view for the selected state
+//    }
+
+
