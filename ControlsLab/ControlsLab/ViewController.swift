e4718a11147ca051404a8aa6b7ef8bdb3317ce5e
melinda <melindad_82@msn.com>
2019-11-22 10:06:04 -0500

    finished controls

--- ControlsLab/ControlsLab/ViewController.swift
+++ ControlsLab/ControlsLab/ViewController.swift
@@ -3,27 +3,56 @@ import UIKit
 class ViewController: UIViewController {
 
     
-   lazy var segmentedControl = UISegmentedControl = {
-        let sc = UISegmentedControl(items: ["♠️", "♣️", "♥️", "♦️"])
-        return sc
-        sc.selectedSegmentIndex = 0
-        sc.addTarget(self, action: #selector(handleSegmentedChange), for: ValueChanged)
-    }()
+//    lazy var segmentedControl: UISegmentedControl = {
+//        let sc = UISegmentedControl(items: ["♠️", "♣️", "♥️", "♦️"])
+//        sc.selectedSegmentIndex = 0
+//        sc.addTarget(self, action: #selector(handledSegmentChange), for: .valueChanged)
+//         return sc
+//    }()
+//
+//    @objc func handledSegmentChange() {
+//
+//    }
     
-    @objc fileprivate func handledSegmentChange() {
-        
-    }
     
-    let club = [String]()
-    let spade = [String]()
-    let heart = [String]()
-    let diamond = [String]()
+    @IBOutlet var cardNumbers: [UILabel]!
+    
+    @IBOutlet var cardSuits: [UILabel]!
+    @IBOutlet weak var suitSelector: UISegmentedControl!
+    
+    
+    @IBOutlet weak var valueStepper: UIStepper!
     
     override func viewDidLoad() {
         super.viewDidLoad()
     
     }
     
+    @IBAction func suitSelector(_ sender: UISegmentedControl) {
+        
+        switch sender.selectedSegmentIndex {
+        case 0:
+            cardSuits.forEach({$0.text = "♠️" })
+        case 1:
+            cardSuits.forEach({$0.text = "♣️" })
+        case 2:
+            cardSuits.forEach({$0.text = "♥️" })
+        case 3:
+            cardSuits.forEach({$0.text = "♦️" })
+        default:
+            break
+            
+        }
+        
+    }
+    
+    
+    @IBAction func valueStepperPressed(_ sender: UIStepper) {
+        cardNumbers.forEach({$0.text = String (Int(sender.value))})
+        switch sender.value {
+        case 11: 
+        }
+    }
     //let stackView = UIStackView(arrangedSubviews: UISegmentedControl)
   
 }
